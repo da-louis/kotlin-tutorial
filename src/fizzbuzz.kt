a207fe1602548fb6f4785b0b067b887495a26a05
da_louis <ruaces@gmail.com>
2018-12-07 22:12:26 +0900

    add fizzbuzz program

---
+++ src/fizzbuzz.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    (1..100).forEach{print("${fb(it)},")}
+}
+
+fun fb(i: Int) : String {
+    when {
+        i % 15 === 0 -> return "fizzbuzz"
+        i % 5 === 0 -> return "buzz"
+        i % 3 === 0 -> return "fizz"
+        else -> return "$i"
+    }
+}
