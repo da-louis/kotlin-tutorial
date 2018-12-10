dd3c93da012ad1b8cf5f51d36ceb5df6d16b2464
da_louis <ruaces@gmail.com>
2018-12-10 22:47:03 +0900

    add square method

--- src/lib/math.kt
+++ src/lib/math.kt
@@ -6,7 +6,12 @@ infix fun Int.myAdd(num:Int):Int{
     return this + num
 }
 
+fun Int.square():Int {
+    return this*this
+}
+
 fun main(args :Array<String>) {
     println(add(123,456))
     println(123 myAdd 456)
+    println(15.square())
 }
