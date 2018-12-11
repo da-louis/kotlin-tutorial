a7614a1554c662d9ff022e4dc1efad8e3c7a5afa
da_louis <ruaces@gmail.com>
2018-12-11 23:04:06 +0900

    learn about import/package

--- src/lib/math.kt
+++ src/lib/math.kt
@@ -1,3 +1,5 @@
+package mylib
+
 fun add(num1:Int, num2: Int):Int {
     return num1 + num2
 }
---
+++ src/main.kt
@@ -0,0 +1,7 @@
+import mylib.add
+import kotlin.math.*
+
+fun main (args:Array<String>) {
+    println(add(123,456))
+    println(PI)
+}
