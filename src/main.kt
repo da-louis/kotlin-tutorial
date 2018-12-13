f6aefef76107286ef2ce28f7d06088a37a8676f3
da_louis <ruaces@gmail.com>
2018-12-13 22:25:40 +0900

    learn about string template

--- src/main.kt
+++ src/main.kt
@@ -4,8 +4,13 @@ val users = mapOf(
         1 to "Jane",
         2 to "John",
         3 to "Mike"
-
 )
+
+val text = """
+string
+template
+test
+"""
 
 fun main (args:Array<String>) {
     println(PI)
@@ -13,4 +18,6 @@ fun main (args:Array<String>) {
     for((id,name) in users){
        println("ID:$id, Name:$name")
     }
+
+    println(text)
 }
