3c372d0e452b4b3dda2e328c1c70b48960ee4b96
da_louis <ruaces@gmail.com>
2018-12-26 22:30:07 +0900

    learn more about for statement

--- src/ForStatement.kt
+++ src/ForStatement.kt
@@ -1,5 +1,7 @@
 fun main(args: Array<String>) {
-    for(index in 1..10){
-        println(index)
-    }
+    for(index in 1..10) println(index)
+
+    for(index in 10 downTo 1) println(index)
+
+    for(index in 1..10 step 2) println(index)
 }
