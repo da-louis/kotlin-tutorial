ed883f606e8ccd1bfd5661f1c300709140beac18
da_louis <ruaces@gmail.com>
2018-12-29 21:45:27 +0900

    learn about arrays

---
+++ src/Array.kt
@@ -0,0 +1,8 @@
+val arrayOfInt: Array<Int> = arrayOf(1, 2, 3)
+val intArray: IntArray = intArrayOf(1, 2, 3)
+
+
+fun main(args: Array<String>) {
+    println(arrayOfInt.toList())
+    println(intArray.toList())
+}
